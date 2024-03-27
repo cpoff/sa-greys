c8fe63612067851ede2555bbed6ced9c2df0853b
C Poff <cpoff@Cs-MacBook-Pro.local>
2024-03-27 15:01:56 -0700

    moved from dev to prod

--- app.py
+++ app.py
@@ -35,30 +35,10 @@ def events():
 def donate():
     return render_template('donate.html')
 
-# Membership page
-@app.route('/membership')
-def membership():
-    return render_template('membership.html')
-
-# Jackie's Fund page
-@app.route('/jackies-fund')
-def jackies_fund():
-    return render_template('jackies-fund.html')
-
-# Volunteer page
-@app.route('/volunteer')
-def volunteer():
-    return render_template('volunteer.html')
-
-# Payments page
-@app.route('/payments')
-def payments():
-    return render_template('payments.html')
-
 # Footer
 @app.context_processor
 def inject_now():
     return {'current_year': datetime.now().year}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
